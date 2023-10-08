dcbf87a417265438e7f02ff7c7290a31998a615c
Anirudh Singh Bhadauria <108342411+AnirudhSinghBhadauria@users.noreply.github.com>
2023-10-08 21:20:44 +0530

    made some more changes

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,4 +1,3 @@
-import { trpc } from "@/app/_trpc/client";
 import { Context } from "@/app/_trpc/context";
 import { TRPCError, initTRPC } from "@trpc/server";
 
@@ -18,3 +17,4 @@ export const router = t.router;
 export const publicProcedure = t.procedure;
 
 export const adminProcedure = publicProcedure.use(isAuth);
+
